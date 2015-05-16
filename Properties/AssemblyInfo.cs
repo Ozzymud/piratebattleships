03d50dd4406901a52300f19e59b654b5b5816092
Ozzymud <ozzymud@gmail.com>
2015-05-16 12:14:42 -0700

    NeutralResourcesLanguage attribute en-US

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -43,6 +43,14 @@ using System.Runtime.InteropServices;
 // Mark the assembly as CLSCompliant for use with all CLS languages
 [assembly: CLSCompliant(true)]
 
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
